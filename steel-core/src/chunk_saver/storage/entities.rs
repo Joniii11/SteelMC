2b26ce1358c41b5441eeb91e06bc03d1ad9bea96
coco875 <59367621+coco875@users.noreply.github.com>
2026-09-22 19:38:12 +0200

    Update entities.rs

--- steel-core/src/chunk_saver/storage/entities.rs
+++ steel-core/src/chunk_saver/storage/entities.rs
@@ -242,6 +242,11 @@ impl ChunkStorage {
         let block_entity_type = REGISTRY.block_entity_types.by_key(block_entity_type_key)?;
 
         if !block_entity_type.is_valid(state.get_block()) {
+            log::warn!(
+                "Skipping block entity {} at {pos:?}: block {} does not accept that type",
+                block_entity_type.key,
+                state.get_block().key,
+            );
             return None;
         }
 
